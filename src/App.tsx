e9fa4fb4bfd0420299a546072553f94cd73ff261
cvirostek <cwvirostek@gmail.com>
2019-07-13 20:44:18 -0700

    Made sticky footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './common/Header';
 import Footer from './common/Footer';
 import AppRouter from './AppRouter';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider, makeStyles, Theme } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 
 const darkTheme = createMuiTheme({
@@ -11,10 +11,20 @@ const darkTheme = createMuiTheme({
     }
 });
 
+const useStyles = makeStyles((theme: Theme) => ({
+    app: {
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh'
+    }
+}));
+
 const App: React.FC = () => {
+    const classes = useStyles();
+
     return (
         <MuiThemeProvider theme={darkTheme}>
-            <div className='App'>
+            <div className={classes.app}>
                 <CssBaseline />
                 <Header />
                 <main>
--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 
-import Typography from '@material-ui/core/Typography';
+import { Theme, Typography, makeStyles, Link } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    footer: {
+        padding: theme.spacing(2),
+        marginTop: 'auto',
+        backgroundColor: theme.palette.background.paper
+    },
+    profileLink: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1)
+    }
+}));
 
 const Footer: React.FC = () => {
+    const classes = useStyles();
+
     return (
-        <footer>
-            <Typography variant='subtitle1'>
-                Personal website of Connor Virostek.
+        <footer className={classes.footer}>
+            <Typography variant='body1' color='textPrimary' align='center'>
+                Personal website of Connor Virostek
+            </Typography>
+            <Typography variant='body2' color='textSecondary' align='center'>
+                <Link className={classes.profileLink} color='inherit' href='https://www.linkedin.com/in/connor-virostek/'>
+                    LinkedIn
+                </Link>
+                |
+                <Link className={classes.profileLink} color='inherit' href='https://github.com/cvirostek'>
+                    GitHub
+                </Link>
             </Typography>
         </footer>
     );
