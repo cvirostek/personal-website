5cecf72ffe4a22ac10733b3ea847fade6d4710c4
cvirostek <cwvirostek@gmail.com>
2019-07-13 22:41:53 -0700

    Added nav buttons to header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+import { Theme, Button, Avatar } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    toolbarItem: {
+        marginRight: theme.spacing(2)
+    }
+}));
 
 const Header: React.FC = () => {
+    const classes = useStyles();
+
     return (
         <header>
             <AppBar position='static' color='inherit'>
                 <Toolbar>
-                    <Typography variant='h6'>
-                        CV
-                    </Typography>
+                    <Avatar className={classes.toolbarItem}>CV</Avatar>
+                    <Button className={classes.toolbarItem} component={RouterLink} to='/'>
+                        Home
+                    </Button>
+                    <Button className={classes.toolbarItem} component={RouterLink} to='/about'>
+                        About
+                    </Button>
                 </Toolbar>
             </AppBar>
         </header>
